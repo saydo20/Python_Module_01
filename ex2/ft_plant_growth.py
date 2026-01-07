14d670d71eb26fed6ec474c2a73a5132e211f6e2
saydo20 <jdiasaad86@gmail.com>
2026-01-07 10:58:47 +0100

    the ex2 is done and ready to push

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -23,33 +23,34 @@ class Plant:
         print(f"{self.name}: {self.height}cm, {self.Age} days old")
 
 
-"""
-creat the Rose object
-"""
-Rose = Plant("Rose", 25, 30)
-first_height = Rose.height
-"""
-a for loop that call the methods get_inof , grow and age i days
-"""
-print("=== Day 1 ===")
-Rose.get_info()
-for i in range(1, 7):
-    Rose.grow()
-    Rose.age()
-print("=== Day 7 ===")
-Rose.get_info()
-print(f"Growth this week: +{Rose.height - first_height}cm")
-print()
-"""
-crear other plant named Sunflower
-"""
-Sunflower = Plant("Sunflower", 80, 40)
-first_height = Sunflower.height
-print("=== Day 1 ===")
-Sunflower.get_info()
-for i in range(1, 7):
-    Sunflower.grow()
-    Sunflower.age()
-print("=== Day 7 ===")
-Sunflower.get_info()
-print(f"Growth this week: +{Sunflower.height - first_height}cm")
+if __name__ == "__main__":
+    """
+    creat the Rose object
+    """
+    Rose = Plant("Rose", 25, 30)
+    first_height = Rose.height
+    """
+    a for loop that call the methods get_inof , grow and age i days
+    """
+    print("=== Day 1 ===")
+    Rose.get_info()
+    for i in range(1, 7):
+        Rose.grow()
+        Rose.age()
+    print("=== Day 7 ===")
+    Rose.get_info()
+    print(f"Growth this week: +{Rose.height - first_height}cm")
+    print()
+    """
+    crear other plant named Sunflower
+    """
+    Sunflower = Plant("Sunflower", 80, 40)
+    first_height = Sunflower.height
+    print("=== Day 1 ===")
+    Sunflower.get_info()
+    for i in range(1, 7):
+        Sunflower.grow()
+        Sunflower.age()
+    print("=== Day 7 ===")
+    Sunflower.get_info()
+    print(f"Growth this week: +{Sunflower.height - first_height}cm")
