75bf4a7e511e7974fc8c78f918b18df559bc3b65
saydo20 <jdiasaad86@gmail.com>
2026-01-07 10:59:12 +0100

    the ex6 is done and ready to push

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -3,9 +3,8 @@ class Plant:
         self.name = name
         self._height = height
         self._age = age
-        # print(f"Plant created: {self.name}")
     """the setter of the hieght and reject the nigative values"""
-    def set_hieght(self, age_value):
+    def set_age(self, age_value):
         if age_value < 0:
             print()
             print("Invalid operation attempted :"
@@ -14,9 +13,9 @@ class Plant:
             print()
         else:
             self._age = age_value
-            print(f"Age updated: {self._height}days [OK]")
+            print(f"Age updated: {self._age}days [OK]")
     """the setter of the age and reject the nigative values"""
-    def set_age(self, height_value):
+    def set_height(self, height_value):
         if height_value < 0:
             print()
             print("Invalid operation attempted :"
@@ -34,11 +33,15 @@ class Plant:
         return self._age
 
 
+"""the flower class that inhiret from the Plant class"""
+
+
 class Flower(Plant):
     def __init__(self, name, _height, _age, color):
+        """use the super function to use the parent constructor"""
         super().__init__(name, _height, _age)
         self.color = color
-
+    """the bloom methode that is special for the flower class"""
     def bloom(self):
         print(f"{self.name} is blooming beautifully!")
         print()
@@ -48,11 +51,16 @@ class Flower(Plant):
               f" {self.get_age()} days, {self.color} color")
 
 
+"""the tree class that inhiret from the Plant class"""
+
+
 class Tree(Plant):
     def __init__(self, name, _height, _age, trunk_diameter):
         super().__init__(name, _height, _age)
         self.trunk_diameter = trunk_diameter
-
+    """
+    the produce shade methode for calculate the shade of a tree
+    """
     def produce_shade(self):
         num = int(self.get_height()) // 100
         print(f"Oak provides {int(3.141592653589793 * (num**2))}"
@@ -64,11 +72,14 @@ class Tree(Plant):
               f" {self.get_age()} days, {self.trunk_diameter}cm diameter")
 
 
+"""the Vegetable class that inhiret from the Plant class"""
+
+
 class Vegetable(Plant):
     def __init__(self, name, _height, _age, harvest_season):
         super().__init__(name, _height, _age)
         self.harvest_season = harvest_season
-
+    """check the season of the harvest and the print what it rich"""
     def nutritional_value(self):
         if self.harvest_season == "summer harvest":
             print(f"{self.name} is rich in vitamin C")
@@ -86,17 +97,18 @@ class Vegetable(Plant):
               f" {self.get_age()} days, {self.harvest_season}")
 
 
-Tulip = Flower("Tulip", 15, 40, "pink")
-Rose = Flower("Rose", 25, 30, "red")
-Oak = Tree("Oak", 500, 1825, 50)
-Oalm = Tree("Palm", 1000, 800, 30)
-Tomato = Vegetable("Tomato", 80, 90, "summer harvest")
-Carrot = Vegetable("Carrot", 120, 30, "winter")
-print("=== Garden Plant Types ===")
-print()
-Rose.get_info()
-Rose.bloom()
-Oak.get_info()
-Oak.produce_shade()
-Tomato.get_info()
-Tomato.nutritional_value()
+if __name__ == "__main__":
+    Tulip = Flower("Tulip", 15, 40, "pink")
+    Rose = Flower("Rose", 25, 30, "red")
+    Oak = Tree("Oak", 500, 1825, 50)
+    Oalm = Tree("Palm", 1000, 800, 30)
+    Tomato = Vegetable("Tomato", 80, 90, "summer harvest")
+    Carrot = Vegetable("Carrot", 120, 30, "winter")
+    print("=== Garden Plant Types ===")
+    print()
+    Rose.get_info()
+    Rose.bloom()
+    Oak.get_info()
+    Oak.produce_shade()
+    Tomato.get_info()
+    Tomato.nutritional_value()
