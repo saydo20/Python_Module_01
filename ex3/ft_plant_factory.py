79cfa1664a3932a0af7d7995c9b09b8eac363b52
saydo20 <jdiasaad86@gmail.com>
2026-01-07 10:58:55 +0100

    the ex3 is done and ready to push

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -25,25 +25,26 @@ class Plant:
         print(f"{self.name} ({self.height}cm, {self.Age} days)")
 
 
-"""
-creat the list that will hold all the objects
-"""
-list = [
-    Plant("Rose", 25, 30),
-    Plant("Oak", 200, 365),
-    Plant("Cacus", 5, 90),
-    Plant("Sunflower", 80, 45),
-    Plant("Fern", 15, 120)
-]
-"""
-a loop for print all the objects
-"""
-print("=== Plant Factory Output ===")
-for i in list:
-    print("Created: ", end="")
-    i.get_info()
-print()
-"""
-get the value of the class variable
-"""
-print(f"Total plants created: {Plant.Total_plants}")
+if __name__ == "__main__":
+    """
+    creat the list that will hold all the objects
+    """
+    list = [
+        Plant("Rose", 25, 30),
+        Plant("Oak", 200, 365),
+        Plant("Cacus", 5, 90),
+        Plant("Sunflower", 80, 45),
+        Plant("Fern", 15, 120)
+    ]
+    """
+    a loop for print all the objects
+    """
+    print("=== Plant Factory Output ===")
+    for i in list:
+        print("Created: ", end="")
+        i.get_info()
+    print()
+    """
+    get the value of the class variable
+    """
+    print(f"Total plants created: {Plant.Total_plants}")
